3074ce6bfe4bcf3d60a4e2ecfd3758921f098d7d
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-07-31 09:47:03 +0300

    setupServer edited

--- src/server.js
+++ src/server.js
@@ -18,15 +18,15 @@ export function setupServer() {
     res.status(404).json({ message: 'Not found' });
   });
 
+  app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
   app.use((err, req, res, next) => {
     res.status(500).json({
       message: 'Something went wrong',
       error: err.message,
     });
-  });
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
   });
 }
 
