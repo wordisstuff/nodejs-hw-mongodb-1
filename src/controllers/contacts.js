77c57df39950f1d50ae892202bec13d504dd16b0
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-08-08 16:28:44 +0300

    updated controllers contacts

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,5 @@
 import { ContactCollection } from '../models/contact.js';
+import createError from 'http-errors';
 
 export const getAllContacts = async (req, res) => {
   const { page = 1, perPage = 10, sortBy = 'name', sortOrder = 'asc', type, isFavourite } = req.query;
@@ -29,4 +30,54 @@ export const getAllContacts = async (req, res) => {
       hasNextPage: page < totalPages,
     },
   });
+};
+
+export const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await ContactCollection.findById(contactId);
+
+  if (!contact) {
+    throw createError(404, 'Contact not found');
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
+
+export const createContact = async (req, res) => {
+  const contact = await ContactCollection.create(req.body);
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: contact,
+  });
+};
+
+export const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await ContactCollection.findByIdAndUpdate(contactId, req.body, { new: true });
+
+  if (!contact) {
+    throw createError(404, 'Contact not found');
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: contact,
+  });
+};
+
+export const deleteContact = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await ContactCollection.findByIdAndDelete(contactId);
+
+  if (!contact) {
+    throw createError(404, 'Contact not found');
+  }
+
+  res.status(204).send();
 };
