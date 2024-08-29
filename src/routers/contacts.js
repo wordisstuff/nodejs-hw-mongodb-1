39ab4dd4a564c31404ca0127dae885cf881fe2f1
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-08-29 13:46:56 +0300

    Routes contacts post&Patch

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,4 +1,5 @@
-import {Router} from 'express';
+import express from 'express';
+
 import {
   getAllContacts,
   getContactById,
@@ -11,10 +12,11 @@ import { validateBody } from '../middlewares/validateBody.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { contactSchema, updateContactSchema } from '../validation/contactSchemas.js';
 import { authenticate } from '../middlewares/authenticate.js';
+import { upload } from '../middlewares/upload.js';
 
 
-const router = Router();
-
+const router = express.Router();
+const jsonParser = express.json();
 // Застосовуємо middleware аутентифікації до всіх маршрутів
 router.use(authenticate);
 
@@ -23,8 +25,8 @@ router.post('/', createContact);
 
 router.get('/', ctrlWrapper(getAllContacts));
 router.get('/:contactId', isValidId, ctrlWrapper(getContactById));
-router.post('/', validateBody(contactSchema), ctrlWrapper(createContact));
-router.patch('/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(updateContact));
+router.post('/', jsonParser,  upload.single('photo'), validateBody(contactSchema), ctrlWrapper(createContact));
+router.patch('/:contactId', isValidId, jsonParser, validateBody(updateContactSchema), ctrlWrapper(updateContact));
 router.delete('/:contactId', isValidId, ctrlWrapper(deleteContact));
 
 export default router;
